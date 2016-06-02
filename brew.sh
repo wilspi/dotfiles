0b40b9e76c830f467bb414ebf8a219320fb73ddb
wilspi <thewilspi@gmail.com>
2016-06-02 17:15:42 +0530

    ADD: brew.sh

---
+++ brew.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Homebrew
+# Install command line tools for osx
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+
+# Formulae / Tool
+# ---------------
+
+# youtube-dl: download videos from youtube and other video sites
+# https://github.com/rg3/youtube-dl/
+brew install youtube-dl
+
+# watch: to run command every x seconds
+# https://en.wikipedia.org/wiki/Watch_(Unix)
+brew install watch
+
+
+
+# Remove outdated versions from the cellar.
+brew cleanup
