4361a38a2a17ab7aef7b592b4838dcfa7fd3e78b
wilspi <thewilspi@gmail.com>
2016-08-02 17:06:22 +0530

    MODIFY: verbose mode, quiet flag

--- scripts/auto_git_pusher.sh
+++ scripts/auto_git_pusher.sh
@@ -8,23 +8,23 @@ verbose=true # Verbose mode
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
 branch_name="(unnamed branch)"     # detached HEAD
 branch_name=${branch_name##refs/heads/}
-if [ "$verbose" = true ] ; then
+if [ "$verbose" = true ]; then
 	echo "You are on branch: " $branch_name
 fi
 
 # Get number of commits ahead
 commits_ahead="$(git rev-list origin/$branch_name..HEAD --count)"
-if [ "$verbose" = true ] ; then
+if [ "$verbose" = true ]; then
 	echo "Commits ahead: " $commits_ahead
 fi
 
 # Get number of commits behind
 commits_behind="$(git rev-list HEAD..origin/$branch_name --count)"
-if [ "$verbose" = true ] ; then
+if [ "$verbose" = true ]; then
 	echo "Commits behind: " $commits_behind
 fi
 if [ "$commits_behind" != "0" ]; then
-	if [ "$verbose" = true ] ; then
+	if [ "$verbose" = true ]; then
 		echo "Behind origin by " $commits_behind " commits. Please pull your branch."
 	fi
 fi
@@ -38,19 +38,23 @@ fi
 
 # Push commits
 if [ $(( $commits_ahead-$push_commits )) -gt "0" ]; then
-	git checkout HEAD~$(( $commits_ahead-$push_commits ))
+	git checkout HEAD~$(( $commits_ahead-$push_commits )) --quiet
 	commit_sha="$(git rev-parse HEAD)"
-	if [ "$verbose" = true ] ; then
+	if [ "$verbose" = true ]; then
 		echo "At commit: " $commit_sha " Pushing commits: " $push_commits
 	fi
-	git push origin $commit_sha:$branch_name
+	git push origin $commit_sha:$branch_name --quiet
 else
-	echo "Origin is upto date."
+	if [ "$verbose" = true ]; then
+		echo "Origin is upto date."
+	fi
 fi
 
 # Back to where HEAD was
-git checkout $branch_name
+git checkout $branch_name --quiet
 
 # Show Git log graph
-echo "Git Log: "
-git log --pretty=oneline --abbrev-commit --graph --decorate --all
+if [ "$verbose" = true ]; then
+	echo "Git Log: "
+	git log --pretty=oneline --abbrev-commit --graph --decorate --all
+fi
