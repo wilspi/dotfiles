e0aac503793576fab0df0001d1dbe709e29c9117
wilspi <thewilspi@gmail.com>
2016-08-02 16:37:35 +0530

    ADD: sshfs package

--- brew.sh
+++ brew.sh
@@ -10,8 +10,13 @@ brew update
 brew upgrade --all
 
 
+
+
 # Formulae / Tool
 # ---------------
+
+# sshfs: mount remote file systems over SSH
+brew install homebrew/fuse/sshfs
 
 # youtube-dl: download videos from youtube and other video sites
 # https://github.com/rg3/youtube-dl/
@@ -20,6 +25,8 @@ brew install youtube-dl
 # watch: to run command every x seconds
 # https://en.wikipedia.org/wiki/Watch_(Unix)
 brew install watch
+
+
 
 
 
