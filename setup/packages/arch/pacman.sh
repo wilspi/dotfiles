629296978d94fcaa911cbc820de2d8884a2a3a00
wilspi <thewilspi@gmail.com>
2020-07-17 02:30:35 +0530

    zsh

--- setup/packages/arch/pacman.sh
+++ setup/packages/arch/pacman.sh
@@ -5,6 +5,7 @@ sudo pacman -Syu
 sudo pacman -S xorg xorg-server gnome gnome-extra
 #sudo pacman -S xf86-video nvidia nvidia-lts amd-ucode pulseaudio pulseaudio-alsa webext-ublock-origin
 sudo pacman -S guake git bash-completion chromium terminator vnstat ntfs-3g pacman-contrib docker firefox-ublock-origin atom spotify
+sudo pacman -S zsh
 
 # Install yay
 #git clone https://aur.archlinux.org/yay.git
